9d0d0d86a0f080caad916b716708f57207e9f326
eramoss <edulramos@outlook.com>
2023-08-22 23:28:57 -0300

    created function to init arena

--- src/arena.h
+++ src/arena.h
@@ -21,6 +21,30 @@ namespace Arena {
   // arena layout
   string Map[arena_height][arena_width];
 
+  void initialize_map() {
+    srand(time(NULL));
+    // Initialize the Map borders
+    for (int y = 0; y < arena_height; ++y) {
+      Map[y][0] = wall_symbol;
+      Map[y][arena_width - 1] = wall_symbol;
+    }
+    for (int x = 0; x < arena_width; ++x) {
+      Map[0][x] = wall_symbol;
+      Map[arena_height - 1][x] = wall_symbol;
+    }
+
+    // Create the Map layout
+    for (int y = 1; y < arena_height - 1; ++y) {
+      for (int x = 1; x < arena_width - 1; ++x) {
+        if (y % 2 == 0 && x % 2 == 0 || rand() % 7 == 0) {
+          Map[y][x] = wall_symbol;
+        }
+        else {
+          Map[y][x] = empty_symbol;
+        }
+      }
+    }
+  }
 
   void print_map() {
     clear_console();
