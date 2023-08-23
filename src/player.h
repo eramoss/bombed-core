91cad107cd3d994ea5339b1259eb98612c8443c5
eramoss <edulramos@outlook.com>
2023-08-22 23:42:47 -0300

    added default namespace to player

--- src/player.h
+++ src/player.h
@@ -1,13 +1,14 @@
-#include "maze.h"
+#include "arena.h"
 
 namespace player {
+  using namespace Arena;
   // Player position
   int player_x = 1;
   int player_y = 1;
 
   bool can_move(int x, int y) {
-    if (x < 0 || y < 0 || x >= maze::maze_width || y >= maze::maze_height) return false;
-    return maze::Map[y][x] == maze::empty_symbol;
+    if (x < 0 || y < 0 || x >= arena_width || y >= arena_height) return false;
+    return Map[y][x] == empty_symbol;
   }
 
   void move_player(int dx, int dy) {
@@ -15,14 +16,14 @@ namespace player {
     int newY = player_y + dy;
 
     if (can_move(newX, newY)) {
-      maze::Map[player_y][player_x] = maze::empty_symbol;
+      Map[player_y][player_x] = empty_symbol;
       player_x = newX;
       player_y = newY;
-      maze::Map[player_y][player_x] = maze::player_symbol;
+      Map[player_y][player_x] = player_symbol;
     }
   }
 
   void init_player() {
-    maze::Map[player_x][player_y] = maze::player_symbol;
+    Map[player_x][player_y] = player_symbol;
   }
 }
