a1d369e99c44989e9af55cb1945ae171bf170b33
eramoss <edulramos@outlook.com>
2023-08-24 01:15:50 -0300

    added more constants chars

--- src/services/consts.h
+++ src/services/consts.h
@@ -8,7 +8,13 @@ const char c_player_symbol = char(2);
 const char* player_symbol = &c_player_symbol;
 const char c_empty_symbol = ' ';
 const char* empty_symbol = &c_empty_symbol;
-
+const char c_explosion = char(176);
+const char* explosion = &c_explosion;
+const char c_great_explosion = char(177);
+const char* great_explosion = &c_great_explosion;
+const char c_greater_explosion = char(178);
+const char* greater_explosion = &c_greater_explosion;
+const char* bomb_character = "@";
 
 #else
 
@@ -17,12 +23,10 @@ const char* empty_symbol = &c_empty_symbol;
 const char* strong_wall_symbol = "\u2588";
 const char* player_symbol = "\U0001FBC7";
 const char* empty_symbol = " ";
-
+const char* explosion = "\u2591";
+const char* great_explosion = "\u2592";
+const char* greater_explosion = "\u2593";
+const char* bomb_character = "\u06de";
 
 #endif // _WIN32
 
-
-const char* explosion = "\U0001BAC7";
-const char* great_explosion = "\U0001BAC7";
-const char* greater_explosion = "\U0001BAC7";
-const char* bomb_character = "@";
