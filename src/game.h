f16f8b1d604e927d8afd81963eaec535ff276e56
eramoss <edulramos@outlook.com>
2023-08-26 16:15:29 -0300

    created interface

--- src/game.h
+++ src/game.h
@@ -3,8 +3,16 @@
 namespace game {
 
   // functions interface*
+
   void check_bomb_ticks_to_destroy();
   void destroy_bomb();
+  void animate_bomb();
+  void destroy_bomb();
+
+  void check_player_death();
+
+  void game_over();
+
 
   void check_bomb_ticks_to_destroy() {
     if (bomb::bomb_enabled) {
@@ -17,27 +25,33 @@ namespace game {
   }
 
   void destroy_bomb() {
-    for (int sprite_index = 0; sprite_index < AMOUNT_OF_SPRITES; sprite_index++) {
-      bomb::create_sprite_animation(bomb::bomb_x, bomb::bomb_y, Arena::Map, bomb::sprite_animations[sprite_index]);
-      Arena::print_map();
-      usleep(50000);
-    }
-    for (int sprite_index = 0; sprite_index < AMOUNT_OF_SPRITES; sprite_index++) {
-      bomb::create_sprite_animation(bomb::bomb_x, bomb::bomb_y, Arena::Map, bomb::sprite_animations[sprite_index]);
-      Arena::print_map();
-      usleep(50000);
-    }
+    animate_bomb();
     bomb::destroy_bomb(Arena::Map);
   }
 
-  void game_over() {
-    exit(0);
+  void animate_bomb() {
+    for (int sprite_index = 0; sprite_index < AMOUNT_OF_SPRITES; sprite_index++) {
+      bomb::create_sprite_animation(bomb::bomb_x, bomb::bomb_y, Arena::Map, bomb::sprite_animations[sprite_index]);
+      Arena::print_map();
+      usleep(50000);
+    }
+    for (int sprite_index = 0; sprite_index < AMOUNT_OF_SPRITES; sprite_index++) {
+      bomb::create_sprite_animation(bomb::bomb_x, bomb::bomb_y, Arena::Map, bomb::sprite_animations[sprite_index]);
+      Arena::print_map();
+      usleep(50000);
+    }
   }
+
+
   void check_player_death() {
     if (Arena::Map[player::player_y][player::player_x] != player_symbol) {
       game_over();
     }
-    if ((enemy_mirror::enemy_x == player::player_x && enemy_mirror::enemy_y == player::player_y) && !enemy_mirror::defeated(Arena::Map)) game_over();
   }
 
+  void game_over() {
+    Arena::print_map();
+    sleep(1);
+    exit(0);
+  }
 }
