860fd32b0121136c199346b69717203cb68974bd
eramoss <edulramos@outlook.com>
2023-10-09 14:51:29 -0300

    put couts in every tests

--- tests/Character.cpp
+++ tests/Character.cpp
@@ -4,6 +4,7 @@
 
 #include "../src/Characters/Character.h"
 #include "assert.h"
+#include "iostream"
 
 void move(){
     Character * character_mock = new Character(Coord{1,1});
@@ -12,12 +13,14 @@ void move(){
 
     assert(character_mock->get_coord().X == 2);
     assert(character_mock->get_coord().Y == 2);
+    std::cout << "Move character passed." << std::endl;
 }
 
 void kill(){
     Character * character_mock = new Character(Coord{1,1});
     character_mock->kill();
     assert(character_mock->defeated());
+    std::cout << "Kill character passed." << std::endl;
 }
 
 int main() {
--- tests/Player.cpp
+++ tests/Player.cpp
@@ -2,6 +2,7 @@
 // Created by eramoss on 29/09/23.
 //
 #include "../src/Characters/Player.h"
+#include "iostream"
 #include <cassert>
 
 void create_bomb_as_active() {
@@ -9,6 +10,7 @@ void create_bomb_as_active() {
     player->put_bomb();
     Bomb bomb = player->get_bomb();
     assert(bomb.active());
+    std::cout << "Create bomb activate passed." << std::endl;
 }
 void create_bomb_in_player_place() {
     auto * player = new Player(Coord{1,1});
@@ -16,6 +18,7 @@ void create_bomb_in_player_place() {
     Bomb bomb = player->get_bomb();
 
     assert(player->get_coord() == bomb.get_coord());
+    std::cout << "Create in position passed." << std::endl;
 }
  int main(){
     create_bomb_as_active();
