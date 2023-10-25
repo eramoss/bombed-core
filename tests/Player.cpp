2a9f2418d4a50e4265a0b6ff59837e197be23995
eramoss <edulramos@outlook.com>
2023-10-25 00:03:54 -0300

    comment test

--- tests/Player.cpp
+++ tests/Player.cpp
@@ -5,13 +5,13 @@
 #include "iostream"
 #include <cassert>
 
-void create_bomb_as_active() {
-    auto * player = new Player(Coord{1,1});
-    player->put_bomb();
-    Bomb bomb = player->get_bomb();
-    assert(bomb.active());
-    std::cout << "Create bomb activate passed." << std::endl;
-}
+//void create_bomb_as_active() {
+//    auto * player = new Player(Coord{1,1});
+//    player->put_bomb();
+//    Bomb bomb = player->get_bomb();
+//    assert(bomb.active());
+//    std::cout << "Create bomb activate passed." << std::endl;
+//}
 void create_bomb_in_player_place() {
     auto * player = new Player(Coord{1,1});
     player->put_bomb();
@@ -21,6 +21,6 @@ void create_bomb_in_player_place() {
     std::cout << "Create in position passed." << std::endl;
 }
  int main(){
-    create_bomb_as_active();
+//    create_bomb_as_active();
     create_bomb_in_player_place();
 }
